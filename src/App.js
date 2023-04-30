7a949d4b3d37c9a88ff0784035452417ade87248
chanfullstack <125750567+chanfullstack@users.noreply.github.com>
2023-04-30 20:25:46 +0700

    Edit package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import DetailPage from './pages/DetailPage/DetailPage.jsx';
 import RandomPage from './pages/RandomPage/RandomPage.jsx';
 import HomePage from './pages/homepage/HomePage.jsx'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/:MealID' element={<DetailPage />} />
-        <Route path='/Random' element={<RandomPage />} />
-
-      </Routes>
-
-
-    </>
+    <div>
+      <BrowserRouter basename='/foodrecipe'>
+        <Routes>
+          <Route path='/foodrecipe' element={<HomePage />} />
+          <Route path='/:MealID' element={<DetailPage />} />
+          <Route path='/Random' element={<RandomPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
 
   );
 }
